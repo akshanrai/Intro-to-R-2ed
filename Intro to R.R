284c781fbae92af8b89b404885334287174d0cbf
Akshan <akr424@lehigh.edu>
2020-09-03 11:37:11 -0400

    Edited Header

--- Intro to R.R
+++ Intro to R.R
@@ -4,6 +4,6 @@
 
 # You will see that you can put non-executable "comment" lines
 # in your script by putting "#" at the beginning of the lines.
-# 
+# AkshanRai20
 # In fact, you can put comments anywhere on a line as long as 
 # you put "#" before them.
